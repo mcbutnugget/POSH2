4ac9b095609db04e331313199654570c47ffce0d
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2024-05-26 22:12:16 +0000

    updated main webpage

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 let tabs = document.querySelectorAll(".tab"); // Use querySelectorAll to get all elements with the class "tab"
 
 tabs.forEach(async tab => {
-  tab.addEventListener("click", () => {
+  tab.addEventListener("click", async () => {
     // 1. Remove "enabled" class from all tabs
     tabs.forEach(otherTab => {
-      otherTab.id.remove("enabled");
+      otherTab.id= "";
     });
 
     // 2. Add "enabled" class to the clicked tab
-    tab.id.add("enabled");
+    tab.id = "enabled";
+    await reload();
   });
-  await reload();
 });
 
 async function reload(){
-    const currentTab = document.getElementById("enabled").innerHTML;
+    var currentTab = document.getElementById("enabled").innerHTML;
     let page = document.getElementById("content");
 
     if(currentTab=="home"){
-        page.innerHTML = await readDisk("https://www.paperproductions.org/html/home.html");
+        page.innerHTML = await readDisk("/html/home.html");
+        POSH.shellStyles.background  = "rgb(50,30,40)";
+    }else if(currentTab=="our products"){
+        page.innerHTML = await readDisk("/html/products.html");
+        POSH.shellStyles.background  = "rgb(30,40,50)";
+    }else if(currentTab=="about us"){
+        page.innerHTML = await readDisk("/html/help.html");
+        POSH.shellStyles.background  = "rgb(30,30,30)";
     }
-
+    console.log(currentTab);
 }
+reload();
+
+const product = {
+  POSH:()=>{
+
+  }
+}
+
+
+
 
 
